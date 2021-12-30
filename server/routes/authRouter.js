f0a299fa00c26dfbb95184044c470b2be581105c
QuinntonCarter <qdcarter92@protonmail.com>
2021-12-29 17:24:08 -0700

    ..

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
+require("dotenv").config();
 const authRouter = express.Router();
 const User = require('../models/user.js');
-require("dotenv").config();
 const jwt = require('jsonwebtoken');
 
 const {
