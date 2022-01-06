7cc4781293ce8987897296621a51bcbaebaedb51
QuinntonCarter <qdcarter92@protonmail.com>
2022-01-06 12:50:15 -0700

    ..

--- client/src/components/context/userProvider.js
+++ client/src/components/context/userProvider.js
@@ -17,8 +17,7 @@ userAxios.interceptors.request.use(config => {
 
 export default function UserProvider(props){
     const initState = {
-        // user: JSON.parse(localStorage.getItem('user')) || null,
-        user: {},
+        user: JSON.parse(localStorage.getItem('user')) || null,
         token: localStorage.getItem('token') || '',
         lists: [],
         recentMood: [],
