644aac03b42f263fec6215c3be578ffcacd96356
QuinntonCarter <qdcarter92@protonmail.com>
2022-01-07 13:18:48 -0700

    ..

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -108,8 +108,8 @@ export default function Auth(){
                 By using this app, you are agreeing to allow it to access your <span style={{color: '#1DB954'}}> Spotify </span> listening history and stats. 
                 If you choose to post, you are agreeing to store the associated <span style={{color: '#1DB954'}}> Spotify </span> listening metadata for viewing by 
                 yourself and friends but no sensitive account information is used in the process. <br/> <span className='text-cerise-600'> This app will never access or store sensitive account information. </span> <br/> You may delete your account at any time.</p>
-            {/* <a type='button' className='btnbold-small bg-indigo-600' href={REACT_APP_SPOTIFY_AUTH}> Login with Spotify </a> */}
+            <a type='button' className='btnbold-small bg-indigo-600' href={REACT_APP_SPOTIFY_AUTH}> Login with Spotify </a>
             {/* for testing */}
-            <a type='button' className='btnbold-small bg-indigo-600' href={`http://localhost:8888/login`}> Login with Spotify </a>
+            {/* <a type='button' className='btnbold-small bg-indigo-600' href={`http://localhost:8888/login`}> Login with Spotify </a> */}
         </div>
 };
